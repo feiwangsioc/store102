c8805d80718bcbdc2d42338b2166f6a10071bd1f
Fei Wang <feiwangsioc@gmail.com>
2020-01-23 02:10:56 +0000

    empty

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,13 @@ Rails.application.routes.draw do
    end
  
   
-  resources :carts
+   resources :carts do
+     collection do
+       delete :clean
+     end
+   end
+   
+   resources :cart_items
   
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
