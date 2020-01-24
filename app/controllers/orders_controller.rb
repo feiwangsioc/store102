9c09805a3560366d68c7432b3e97abfcd80207d0
Fei Wang <feiwangsioc@gmail.com>
2020-01-24 01:09:10 +0000

    add payment_method

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,12 @@
 class OrdersController < ApplicationController
 
    before_action :authenticate_user!, only: [:create]
+   
+   def show
+    #  @order = Order.find(params[:id])
+      @order = Order.find_by_token(params[:id])
+     @product_lists = @order.product_lists
+   end 
 
    def create
      @order = Order.new(order_params)
@@ -8,11 +14,37 @@ class OrdersController < ApplicationController
      @order.total = current_cart.total_price
 
      if @order.save
-       redirect_to order_path(@order)
+         current_cart.cart_items.each do |cart_item|
+         product_list = ProductList.new
+         product_list.order = @order
+         product_list.product_name = cart_item.product.title
+         product_list.product_price = cart_item.product.price
+         product_list.quantity = cart_item.quantity
+         product_list.save
+       end          
+         
+       
+       redirect_to order_path(@order.token)
      else
        render 'carts/checkout'
      end
    end
+   
+  def pay_with_alipay
+    @order = Order.find_by_token(params[:id])
+    @order.set_payment_with!("alipay")
+    @order.pay!
+
+    redirect_to order_path(@order.token), notice: "使用支付宝成功完成付款"
+  end
+
+  def pay_with_wechat
+    @order = Order.find_by_token(params[:id])
+    @order.set_payment_with!("wechat")
+    @order.pay!
+
+    redirect_to order_path(@order.token), notice: "使用微信成功完成付款"
+  end
 
    private
 
